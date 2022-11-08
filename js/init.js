3314920e4e66173e09ca1c5a987f575a9b2fc0eb
autenticahrm <117742266+autenticahrm@users.noreply.github.com>
2022-11-08 08:13:28 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyBKXKRaQCWdfiSCZR39wunK0QApZMhgeqE",
-    authDomain: "pauten43.firebaseapp.com",
-    projectId: "pauten43",
-    storageBucket: "pauten43.appspot.com",
-    messagingSenderId: "167024006136",
-    appId: "1:167024006136:web:2766577637b6ab37c43c0d",
-    measurementId: "G-S4KD63PHHW"
+    apiKey: "AIzaSyAX6xNnOjhP7teaIVWCpkmNzqs789Xp2VA",
+  authDomain: "autenticahrm1.firebaseapp.com",
+  projectId: "autenticahrm1",
+  storageBucket: "autenticahrm1.appspot.com",
+  messagingSenderId: "338034335963",
+  appId: "1:338034335963:web:4f870713fba6a3c0852cfe",
+  measurementId: "G-PNZTSW8G4X"
     });
